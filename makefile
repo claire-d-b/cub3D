fe4d1cf2ee032118b8e05d422d2e248ec7cc980c
Claire DE BERNY <clde-ber@e1r11p16.42.fr>
2020-08-17 17:11:55 +0200

    diag

--- makefile
+++ makefile
@@ -6,7 +6,7 @@
 #    By: clde-ber <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/14 11:48:01 by clde-ber          #+#    #+#              #
-#    Updated: 2020/08/17 15:47:03 by clde-ber         ###   ########.fr        #
+#    Updated: 2020/08/17 16:13:39 by clde-ber         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -39,7 +39,7 @@ NAME		= Cub3D
 CC		= gcc
 RUN_SRC			= -c
 RUN_O			= -o
-INCL		= -L. cminilibx/libmlx.a -lXext -lX11 -lm -lbsd
+INCL		= -I minilibx_opengl_20191021 -L minilibx_opengl_20191021 -l mlx -framework OpenGL -framework AppKit
 RM		= rm -f
 CFLAGS		= -g3 -fsanitize=address -Wall -Wextra -Werror
 $(NAME):	$(OBJ)
--- moverl.c
+++ moverl.c
@@ -6,7 +6,7 @@
 /*   By: clde-ber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/22 08:28:53 by clde-ber          #+#    #+#             */
-/*   Updated: 2020/08/17 15:45:32 by clde-ber         ###   ########.fr       */
+/*   Updated: 2020/08/17 17:11:00 by clde-ber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,116 +14,172 @@
 
 void walk_right_teta_pos(t_player *player)
 {
-   if (player->teta >= 0 && player->teta < M_PI / 4)
-    	player->y = (player->map[(int)(player->x / CELL_SIZE) - 1][(int)((player->y - CELL_SIZE) / 
-		CELL_SIZE) - 1] != '1' && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->y - CELL_SIZE : player->y;
-	else if (player->teta >= M_PI / 4 && player->teta < M_PI / 1.25)
-			player->x = (player->map[(int)((player->x - CELL_SIZE) / CELL_SIZE) - 1][(int)(player->y / 
+printf("player->teta %f\n", player->teta);
+printf("right");
+	if (get_decimals(player->teta) == get_decimals(M_PI/4))
+	{
+		player->x = (player->map[(int)((player->x - CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y - CELL_SIZE) / 
 		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
-		CELL_SIZE) < player->table_lenght) ? player->x - CELL_SIZE : player->x;
+		CELL_SIZE) <= player->table_lenght && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->x - CELL_SIZE : player->x;
+		player->y = (player->map[(int)((player->x - CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y - CELL_SIZE) / 
+		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
+		CELL_SIZE) <= player->table_lenght && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->y - CELL_SIZE : player->y;
+	}
+	else if (get_decimals(player->teta) == get_decimals((M_PI/4)*3))
+	{
+		player->x = (player->map[(int)((player->x - CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->x - CELL_SIZE : player->x;
+		player->y = (player->map[(int)((player->x - CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->y + CELL_SIZE : player->y;
+	}
 	else
+	{
+   		if (player->teta >= 0 && player->teta < M_PI / 4)
+    		player->y = (player->map[(int)(player->x / CELL_SIZE) - 1][(int)((player->y - CELL_SIZE) / 
+			CELL_SIZE) - 1] != '1' && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
+			CELL_SIZE) <= player->max) ? player->y - CELL_SIZE : player->y;
+		else if (player->teta >= M_PI / 4 && player->teta < (M_PI / 4) * 3)
+			player->x = (player->map[(int)((player->x - CELL_SIZE) / CELL_SIZE) - 1][(int)(player->y / 
+			CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
+			CELL_SIZE) <= player->table_lenght) ? player->x - CELL_SIZE : player->x;
+		else
 			player->y = (player->map[(int)(player->x / CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
-		CELL_SIZE) - 1] != '1' && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->y + CELL_SIZE : player->y;
+			CELL_SIZE) - 1] != '1' && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+			CELL_SIZE) <= player->max) ? player->y + CELL_SIZE : player->y;
+	}
 }
 
 void walk_right_teta_neg(t_player *player)
 {
-    if (player->teta >= -M_PI && player->teta < -M_PI / 1.25)
-			player->y = (player->map[(int)(player->x / CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
-		CELL_SIZE) - 1] != '1' && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->y + CELL_SIZE : player->y;
-	else if (player->teta >= (-M_PI / 1.25) && player->teta < -M_PI / 4)
-			 player->x = (player->map[(int)((player->x + CELL_SIZE) / CELL_SIZE) - 1][(int)(player->y / 
+//printf("player->teta %f\n", player->teta);i
+printf("right neg");
+	if (get_decimals(player->teta) == get_decimals(-M_PI/4))
+	{
+		player->x = (player->map[(int)((player->x - CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->x - CELL_SIZE : player->x;
+		player->y = (player->map[(int)((player->x - CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->y + CELL_SIZE : player->y;
+	}
+	else if (get_decimals(player->teta) == get_decimals(-(M_PI/4)*3))
+	{
+		player->x = (player->map[(int)((player->x + CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
 		CELL_SIZE) - 1] != '1' && (int)((player->x + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x + CELL_SIZE) / 
-		CELL_SIZE) < player->table_lenght) ? player->x + CELL_SIZE : player->x;
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->x + CELL_SIZE : player->x;
+		player->y = (player->map[(int)((player->x + CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) - 1] != '1' && (int)((player->x + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x + CELL_SIZE) / 
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->y + CELL_SIZE : player->y;
+	}
 	else
+	{
+    	if (player->teta >= -M_PI && player->teta < -(M_PI / 4) * 3)
+			player->y = (player->map[(int)(player->x / CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
+			CELL_SIZE) - 1] != '1' && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+			CELL_SIZE) <= player->max) ? player->y + CELL_SIZE : player->y;
+		else if (player->teta >= (-(M_PI / 4) * 3) && player->teta < -M_PI / 4)
+			 player->x = (player->map[(int)((player->x + CELL_SIZE) / CELL_SIZE) - 1][(int)(player->y / 
+			CELL_SIZE) - 1] != '1' && (int)((player->x + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x + CELL_SIZE) / 
+			CELL_SIZE) <= player->table_lenght) ? player->x + CELL_SIZE : player->x;
+		else
 			player->y = (player->map[(int)(player->x / CELL_SIZE) - 1][(int)((player->y - CELL_SIZE) / 
-		CELL_SIZE) - 1] != '1' && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->y - CELL_SIZE : player->y;
+			CELL_SIZE) - 1] != '1' && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
+			CELL_SIZE) <= player->max) ? player->y - CELL_SIZE : player->y;
+	}
 }
 
 void walk_left_teta_pos(t_player *player)
 {
 	printf("player->teta %f\n", player->teta);
+	printf("left");
 	if (get_decimals(player->teta) == get_decimals(M_PI/4))
 	{
 		player->x = (player->map[(int)((player->x + CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
 		CELL_SIZE) - 1] != '1' && (int)((player->x + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x + CELL_SIZE) / 
-		CELL_SIZE) < player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->x + CELL_SIZE : player->x;
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->x + CELL_SIZE : player->x;
 		player->y = (player->map[(int)((player->x + CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
 		CELL_SIZE) - 1] != '1' && (int)((player->x + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x + CELL_SIZE) / 
-		CELL_SIZE) < player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->y + CELL_SIZE : player->y;
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->y + CELL_SIZE : player->y;
 	}
 	else if (get_decimals(player->teta) == get_decimals((M_PI/4)*3))
 	{
-		player->x = (player->map[(int)((player->x + CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y - CELL_SIZE) / 
-		CELL_SIZE) - 1] != '1' && (int)((player->x + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x + CELL_SIZE) / 
-		CELL_SIZE) < player->table_lenght && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->x + CELL_SIZE : player->x;
-		player->y = (player->map[(int)((player->x + CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y - CELL_SIZE) / 
-		CELL_SIZE) - 1] != '1' && (int)((player->x + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x + CELL_SIZE) / 
-		CELL_SIZE) < player->table_lenght && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->y - CELL_SIZE : player->y;
+		player->x = (player->map[(int)((player->x - CELL_SIZE) / CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->x - CELL_SIZE : player->x;
+		player->y = (player->map[(int)((player->x - CELL_SIZE) / CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->y + CELL_SIZE : player->y;
 	}
 	else
 	{
     	if (player->teta >= 0 && player->teta < M_PI / 4)
     		player->y = (player->map[(int)(player->x / CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
 			CELL_SIZE) - 1] != '1' && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
-			CELL_SIZE) < player->max) ? player->y + CELL_SIZE : player->y;
+			CELL_SIZE) <= player->max) ? player->y + CELL_SIZE : player->y;
 		else if (player->teta > M_PI / 4 && player->teta < (M_PI / 4) * 3)
 				player->x = (player->map[(int)((player->x + CELL_SIZE) / CELL_SIZE) - 1][(int)(player->y 
 				 / CELL_SIZE) - 1] != '1' && (int)((player->x + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x + 
-			 	CELL_SIZE) / CELL_SIZE) < player->table_lenght) ? player->x + CELL_SIZE : player->x;
+			 	CELL_SIZE) / CELL_SIZE) <= player->table_lenght) ? player->x + CELL_SIZE : player->x;
 		else
 			player->y = (player->map[(int)(player->x / CELL_SIZE) - 1][(int)((player->y - CELL_SIZE) / 
 			CELL_SIZE) - 1] != '1' && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) /
-			CELL_SIZE) < player->max) ? player->y - CELL_SIZE : player->y;
+			CELL_SIZE) <= player->max) ? player->y - CELL_SIZE : player->y;
 	}
 }
 
 void walk_left_teta_neg(t_player *player)
 {
 	printf("player->teta %f\n", player->teta);
+	printf("left neg");
 	if (get_decimals(player->teta) == get_decimals(-M_PI/4))
 	{
 		player->x = (player->map[(int)((player->x - CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
 		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
-		CELL_SIZE) < player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->x - CELL_SIZE : player->x;
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->x - CELL_SIZE : player->x;
 		player->y = (player->map[(int)((player->x - CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
 		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
-		CELL_SIZE) < player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->y + CELL_SIZE : player->y;
+		CELL_SIZE) <= player->table_lenght && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->y + CELL_SIZE : player->y;
 	}
 	else if (get_decimals(player->teta) == get_decimals(-(M_PI/4)*3))
 	{
 		player->x = (player->map[(int)((player->x - CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y - CELL_SIZE) / 
 		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
-		CELL_SIZE) < player->table_lenght && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->x - CELL_SIZE : player->x;
+		CELL_SIZE) <= player->table_lenght && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->x - CELL_SIZE : player->x;
 		player->y = (player->map[(int)((player->x - CELL_SIZE)/ CELL_SIZE) - 1][(int)((player->y - CELL_SIZE) / 
 		CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
-		CELL_SIZE) < player->table_lenght && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
-		CELL_SIZE) < player->max) ? player->y - CELL_SIZE : player->y;
+		CELL_SIZE) <= player->table_lenght && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
+		CELL_SIZE) <= player->max) ? player->y - CELL_SIZE : player->y;
 	}	
 	else
 	{
 		if (player->teta >= -M_PI && player->teta < -(M_PI / 4) * 3)
 			player->y = (player->map[(int)(player->x / CELL_SIZE) - 1][(int)((player->y - CELL_SIZE) / 
 			CELL_SIZE) - 1] != '1' && (int)((player->y - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y - CELL_SIZE) / 
-			CELL_SIZE) < player->max) ? player->y - CELL_SIZE : player->y;
+			CELL_SIZE) <= player->max) ? player->y - CELL_SIZE : player->y;
 		else if (player->teta > (-(M_PI / 4) * 3) && player->teta < -M_PI / 4)
 			player->x = (player->map[(int)((player->x - CELL_SIZE) / CELL_SIZE) - 1][(int)(player->y / 
 			CELL_SIZE) - 1] != '1' && (int)((player->x - CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->x - CELL_SIZE) / 
-			CELL_SIZE) < player->table_lenght) ? player->x - CELL_SIZE : player->x;
+			CELL_SIZE) <= player->table_lenght) ? player->x - CELL_SIZE : player->x;
 		else
 			player->y = (player->map[(int)(player->x / CELL_SIZE) - 1][(int)((player->y + CELL_SIZE) / 
 			CELL_SIZE) - 1] != '1' && (int)((player->y + CELL_SIZE) / CELL_SIZE) > 1 && (int)((player->y + CELL_SIZE) / 
-			CELL_SIZE) < player->max) ? player->y + CELL_SIZE : player->y;
+			CELL_SIZE) <= player->max) ? player->y + CELL_SIZE : player->y;
 	}
 }
