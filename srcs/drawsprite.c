acbbfba6139fa5d47f2646d72f9847ba212e7aca
clde-ber <claire.deberny@gmail.com>
2020-09-21 12:37:56 +0200

    sprites

--- srcs/drawsprite.c
+++ srcs/drawsprite.c
@@ -14,8 +14,8 @@
 
 void	register_sprite_start(int i, t_player *player, float angle, float d)
 {
-	player->sprite[i][4] = player->ray_x + d * sin(angle);
-	player->sprite[i][5] = player->ray_y + d * cos(angle);
+	player->sprite[i][4] = player->ray_x + (d) * sin(angle);
+	player->sprite[i][5] = player->ray_y + (d) * cos(angle);
 	player->sprite[i][7] = player->struct_screen.i;
 	player->sprite[i][10] = player->struct_screen.x / d;
 	if (player->distance > player->sprite[i][10])
@@ -46,7 +46,38 @@ void	draw_sprite_from_start(t_player *player, int i, int j, int count)
 	(int)(player->sprite[count][3] + (wall_h - (player->sprite[count][3] - player->sprite[count][7])));
 	xposition_start = 
 	(int)(player->sprite[count][7] - (wall_h - (player->sprite[count][3] - player->sprite[count][7])));
-	if (((player->sprite[count][10] - player->sprite[count][9] >= 0 || player->sprite[count][7] == 0) && player->sprite[count][3] != player->struct_screen.x - 1))
+	if (player->sprite[count][7] == 0 || player->sprite[count][3] == player->struct_screen.x - 1)
+	{
+		if ((player->sprite[count][8] == 0) && j <= wall_h && get_decimals(player->sprite[count][5]) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h <= player->ids.xpm_sprite_w && get_decimals(player->sprite[count][5]) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h >= 0 && j >= 0)
+	{
+		color = set_texture_sprite(player, j * player->ids.xpm_sprite_h /
+		wall_h, get_decimals(player->sprite[count][5]) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h);
+	}
+	if ((player->sprite[count][8] == 1) && j <= wall_h && (100 - get_decimals(player->sprite[count][5])) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h <= player->ids.xpm_sprite_w && (100 - get_decimals(player->sprite[count][5])) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h >= 0 && j >= 0)
+	{
+		color = set_texture_sprite(player, j * player->ids.xpm_sprite_h /
+		wall_h, (100 - get_decimals(player->sprite[count][5])) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h);
+	}
+	if ((player->sprite[count][8] == 2) && j <= wall_h && (100 - get_decimals(player->sprite[count][4])) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h <= player->ids.xpm_sprite_w && (100 - get_decimals(player->sprite[count][4])) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h >= 0 && j >= 0)
+	{
+		color = set_texture_sprite(player, j * player->ids.xpm_sprite_h /
+		wall_h, (100 - get_decimals(player->sprite[count][4])) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h);
+	}
+	if ((player->sprite[count][8] == 3) && j <= wall_h && get_decimals(player->sprite[count][4]) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h <= player->ids.xpm_sprite_w && get_decimals(player->sprite[count][4]) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h >= 0 && j >= 0)
+	{
+		color = set_texture_sprite(player, j * player->ids.xpm_sprite_h /
+		wall_h, get_decimals(player->sprite[count][4]) * player->ids.xpm_sprite_w / 100 + i * player->ids.xpm_sprite_w / wall_h);
+	}
+	if (
+	color > 0 && player->sprite[count][7] + i <= player->sprite[count][3]
+		&& player->sprite[count][7] + i >= player->sprite[count][7]
+		&& player->struct_screen.y / 2 - wall_h / 2
+		+ j >= 0 && player->struct_screen.y / 2 - wall_h / 2 + j <
+		player->struct_screen.y)
+			change_color(player, player->struct_screen.y / 2 - wall_h / 2 + j,
+			player->sprite[count][7] + i, color);
+	}
+	if ((player->sprite[count][10] - player->sprite[count][9] >= 0 && player->sprite[count][7] != 0 && player->sprite[count][3] != player->struct_screen.x - 1))
 	{
 	if (j <= wall_h && i <= wall_h && i >= 0 && j >= 0)
 	{
@@ -62,7 +93,7 @@ void	draw_sprite_from_start(t_player *player, int i, int j, int count)
 			change_color(player, player->struct_screen.y / 2 - wall_h / 2 + j,
 			xposition_start + i, color);
 	}
-	else
+	if ((player->sprite[count][10] - player->sprite[count][9] < 0 && player->sprite[count][7] != 0 && player->sprite[count][3] != player->struct_screen.x - 1))
 	{
 	if (j <= wall_h && i <= wall_h && i >= 0 && j >= 0)
 	{
@@ -88,8 +119,8 @@ void	pivot_textures_sprite(int i, int j, t_player *player)
 	sort_sprite(player);
 	while (player->sprite[count] && (int)player->sprite[count][0] != 0)
 	{
-	//	printf("playeri 9 %f\n", player->sprite[count][9]);
-	//	printf("playeri 10 %f\n", player->sprite[count][10]);
+		printf("playeri 8 %f\n", player->sprite[count][8]);
+	//	printf("playeri 5 %f\n", player->sprite[count][5]);
 	//	printf("count %d\n", count);
 	//	printf("player->sprite[count][10] - player->sprite[count][9] %f\n", player->sprite[count][10] - player->sprite[count][9]);
 	//	if (player->sprite[count][10] - player->sprite[count][9] < 0)
